d1f86ca9b0e0d1b5bbaee20eacd5ac562c4dcf56
Ricardo de Deijn <42801543+Neatherblok@users.noreply.github.com>
2020-01-08 21:07:57 +0100

    Fixed contribution lay-out

--- resources/views/member/contribution.blade.php
+++ resources/views/member/contribution.blade.php
@@ -7,29 +7,37 @@
                 <div class="card">
                     <div class="card-body">
                         <h1 align="center">Contributie</h1>
-                        <h4 class="text-center mb-3">De contributieregeling voor de reddingsbrigade is als volgt</h4>
-                        <table class="w-100 mb-0">
+                        <h4 class="text-center mb-4">De contributieregeling voor de reddingsbrigade is als volgt</h4>
+                        <table class="w-100 mb-2">
                             <tr>
                                 <td class="w-50">Jeugdleden:</td>
-                                <td>1 kind: € 48,- per kwartaal</td>
+                                <td></td>
                             </tr>
                             <tr>
-                                <td><strong>Per kind extra uit hetzelfde gezin € 3,- minder per kwartaal</strong></td>
-                                <td>2 kinderen: € 93,- per kwartaal, etc.</td>
+                                <td>1 kind</td>
+                                <td class="text-right">€ 48,- per kwartaal</td>
+                            </tr>
+                            <tr>
+                                <td>2 kinderen</td>
+                                <td class="text-right">€ 93,- per kwartaal</td>
+                            </tr>
+                            <tr>
+                                <td>etc...</td>
                             </tr>
                         </table>
+                        <p><strong>Per kind extra uit hetzelfde gezin € 3,- minder per kwartaal</strong></p>
                         <hr>
                         <table class="w-100">
                             <tr>
                                 <td class="w-50">Gediplomeerde instructeurs: </td>
-                                <td>€ 10,- per kwartaal</td>
+                                <td class="text-right">€ 10,- per kwartaal</td>
                             </tr>
                         </table>
                         <hr>
                         <table class="w-100">
                             <tr>
                                 <td class="w-50">Seniorleden:</td>
-                                <td>€ 48,- per kwartaal</td>
+                                <td class="text-right">€ 48,- per kwartaal</td>
                             </tr>
                         </table>
                     </div>
