13c8d5205cdbdeec3c522aabdc092ee7ee0b914c
Ricardo de Deijn <42801543+Neatherblok@users.noreply.github.com>
2020-06-17 21:29:29 +0200

    Added content to index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,14 +12,12 @@
         <section id="what-we-do" class="what-we-do-section text-center">
             <h2 class="font-weight-bold text-uppercase mx-auto">Wat wij doen</h2>
             <div class="row justify-content-center">
-                <a href="#zwemmend-redden" class="m-3 linkUnderlineless">Zwemmend redden</a>
-                <p class="m-3">|</p>
-                <a href="#theorie" class="m-3 linkUnderlineless">Theorie</a>
-                <p class="m-3">|</p>
-                <a href="#EHBO" class="m-3 linkUnderlineless">EHBO</a>
+                <a href="#zwemmend-redden" class="m-3 btn register-btn linkUnderlineless">Zwemmend redden</a>
+                <a href="#theorie" class="m-3 btn register-btn linkUnderlineless">Theorie</a>
+                <a href="#EHBO" class="m-3 btn register-btn linkUnderlineless">EHBO</a>
             </div>
-            <div id="zwemmend-redden" class="card">
-                <img src="{{ asset('assets/img/reddingsboei.png')}}" class="w-25 m-auto">
+            <div id="zwemmend-redden">
+                <img src="{{ asset('assets/img/rescue_tube.png')}}" class="w-25 m-auto">
                 <h2 align="center">Zwemmend redden</h2>
                 <p>In Terneuzen en omgeving is zo ontzettend veel water dat er makkelijk iemand in terecht kan
                     komen. Als je dan kunt zwemmend redden en dus geleerd en geoefend hebt om iemand uit het
@@ -44,13 +42,37 @@
                     diploma's. Vanaf dit punt zijn er ook theorie lessen.
                 </p>
             </div>
-            <div id="theorie">
+            <div id="theorie" class="card text-white" style="background-color: rgba(255,88,0,0.9); margin-left: -7rem; margin-right: -7rem; padding-left: 6.5rem; padding-right: 6.5rem">
                 <img src="{{ asset('assets/img/theorieboek.png')}}" class="w-25 h-25 m-auto">
                 <h2 align="center">Theorie</h2>
+                <p>In de theorie lessen worden alle technieken nogmaals behandeld maar wordt er dieper op de stof
+                    ingegaan en nieuwe situaties besproken. Hierbij kan je denken aan een auto te water, wat moet je
+                    doen
+                    in dat geval en wat juist niet? Of de stroming van een rivier, hoe ga je hier mee om? In het zwembad
+                    heb je
+                    hier namelijk nooit last van. Wat betekenen nou eigenlijk die vlaggen op het strand die je wel eens
+                    ziet wapperen op een warme zomerdag?
+                    Dit soort situaties worden behandeld tijdens de theorie lessen. Naast de theorie-lessen die wij
+                    aanbieden krijg je ook
+                    een theorieboek om thuis te kunnen leren.<br>
+                    De theorie is een examen onderdeel van het diploma. Bij Life Saver 1 zal de theorie op papier af
+                    genomen worden, maar bij Life Saver 2 en 3 is dit mondeling met de examinator waarbij echte inzicht
+                    in de situatie wordt vereist.
+                </p>
             </div>
-            <div id="EHBO" class="card">
+            <div id="EHBO">
                 <img src="{{ asset('assets/img/EHBO_koffer.png')}}" class="w-25 m-auto">
                 <h2 align="center">EHBO</h2>
+                <p>Naast de theorie over het Reddend Zwemmen zijn er ook lessen in E.H.B.O voor Life Saver 2 en 3. In
+                    deze lessen krijg je zowel praktijk als theorie les. Tijdens de praktijk leer je bijvoorbeeld een
+                    verband aan te leggen, de stabiele zijligging en reanimeren. Tijdens de theorie krijg je uitleg over
+                    hoe ons lichaam reageert op kou en warmte, wat is nou het verschil tussen de soorten verband of wat
+                    moet je doen bij iemand met een flauwte.<br>
+                    Beide lessen worden in het sportcentrum de Vliegende Vaart gegeven, of in overleg met de leerlingen
+                    op een andere locatie. Het E.H.B.O diploma horen de leerlingen al in bezit te hebben voor het
+                    theorie en praktijk examen, maar hier helpen wij uiteraard aan mee. Deze datum wordt ruim voor de
+                    theorie en praktijk examen gepland zodat een herexamen eventueel mogelijk is.
+                </p>
             </div>
         </section>
     </div>
